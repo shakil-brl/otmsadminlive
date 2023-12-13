7e691085470ab2dd81c46f30cc2611e7f98639c7
Ashraful-NG <ashraful@newgen-bd.com>
2023-12-13 17:31:10 +0600

    detail dashboard and bug fixing

--- resources/views/dashboard_details/complete_batches.blade.php
+++ resources/views/dashboard_details/complete_batches.blade.php
@@ -1,54 +1,59 @@
 @extends('layouts.auth-master')
-
+{{-- @dd($running_batches); --}}
 @section('content')
-<!--begin::Content-->
-<div class="m-5">
-    <h3>{{ __('batch-list.complete_batches') }}</h3>
-    <x-alert />
-
-    <table class="table table-bordered bg-white">
-        <thead>
-            <th>{{ __('batch-list.sl') }}</th>
-            <th>{{ __('batch-list.batch_code') }}</th>
-            <th>{{ __('batch-list.course_name') }}</th>
-            <th>{{ __('batch-list.location') }}</th>
-            <th>{{ __('batch-list.start_date') }}</th>
-            <th>{{ __('batch-list.total_class') }}</th>
-            <th>{{ __('batch-list.class_schedule') }}</th>
-            <th>{{ __('batch-list.action') }}</th>
-        </thead>
-        <tbody>
-            @foreach (range(1, 5) as $batch)
-            <tr>
-                <td>
-                    {{ $loop->iteration }}
-                </td>
-                <td>
-                    {{ fake()->name() }}
-                </td>
-                <td>
-                    {{ fake()->name() }}
-                </td>
-                <td>
-                    {{ fake()->city() }}
-                </td>
-                <td>
-                    {{ fake()->time() }}
-                </td>
-                <td>
-                    {{ rand(0, 50) }}
-                </td>
-                <td>
-                    {{ fake()->date() }}
-                </td>
-                <td>
-                    <a href="" class="btn btn-sm btn-info">
-                        {{ __('batch-list.view') }}
-                    </a>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+    <!--begin::Content-->
+    <div class="m-5">
+        <h3>{{ __('batch-list.running_batches') }}</h3>
+        <x-alert />
+        @isset($complete_batches)
+            <div class="my-3">
+                <form action="">
+                    <div class="w-50 d-flex gap-3">
+                        <input type="search" name="search" value="{{ request('search') }}" class="form-control w-75"
+                            placeholder="{{ __('batch-list.search_here') }}">
+                        <input type="submit" class="form-control btn btn-primary w-25" value="{{ __('batch-list.search') }}">
+                    </div>
+                </form>
+            </div>
+            <table class="table table-bordered bg-white">
+                <thead>
+                    <th>{{ __('batch-list.sl') }}</th>
+                    <th>{{ __('batch-list.batch_code') }}</th>
+                    <th>{{ __('batch-list.start_date') }}</th>
+                    <th>{{ __('batch-list.course_name') }}</th>
+                    <th>{{ __('batch-list.location') }}</th>
+                    <th>{{ __('batch-list.development_partner') }}</th>
+                </thead>
+                <tbody>
+                    @foreach (collect($complete_batches) as $batch)
+                        <tr>
+                            <td>
+                                {{ $loop->iteration }}
+                            </td>
+                            <td>
+                                {{ $batch['training_batch'] ? $batch['training_batch']['batchCode'] : '' }}
+                            </td>
+                            <td>
+                                {{ $batch['class_time'] ?? '' }}
+                            </td>
+                            <td>
+                                {{ $batch['training_batch'] ? $batch['training_batch']['training']['title']['Name'] : '' }}
+                            </td>
+                            <td>
+                                {{ $batch['training_batch'] ? $batch['training_batch']['GEOLocation'] : '' }}
+                            </td>
+                            <td>
+                                {{ $batch['training_batch'] ? $batch['training_batch']['provider']['name'] : '' }}
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            {!! $paginator->links() !!}
+        @endisset
+    </div>
+    <!--end::Content-->
+@section('script')
+    <script></script>
+@endsection
 @endsection
