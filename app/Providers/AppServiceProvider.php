9e4172ff786701459ac89a36922efa66775eac4e
tariqul-babylon <tariqul@newgen-bd.com>
2023-12-18 18:25:42 +0600

    df

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::directive('row', function () {
+            return "<div class='row row-form row-cols-1 row-cols-md-2 row-cols-lg-3 g-3 row-cols-xl-4'>";
+        });
+
+        Blade::directive('endrow', function () {
+            return "</div>";
+        });
+        Paginator::useBootstrap();
     }
 }
