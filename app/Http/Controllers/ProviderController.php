48dbc164f12c6513298a2e6059ca26ee8346a027
Ashraful-NG <ashraful@newgen-bd.com>
2023-12-23 21:41:06 +0600

    random

--- app/Http/Controllers/ProviderController.php
+++ app/Http/Controllers/ProviderController.php
@@ -2,24 +2,78 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Clients\ApiHttpClient;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ProviderController extends Controller
 {
     // show ui for all provider item
-    public function index()
+    public function index(Request $request)
     {
-        return view('providers.index');
+        $provider_results = ApiHttpClient::request('get', 'partnerslist', [
+            'page' => $request->page ?? 1,
+            'search' => $request->search,
+        ])->json();
+
+        // dd($results);
+        if ($provider_results['success'] == true) {
+            $providers = $provider_results['data']['data'];
+            $paginator = $this->customPaginate($provider_results, $request, route('providers.index'));
+
+            return view('providers.index', ['providers' => $providers, 'paginator' => $paginator]);
+        } else {
+            session()->flash('type', 'Danger');
+            session()->flash('message', 'Something went wrong');
+            return redirect()->back();
+        }
     }
 
     // show ui for induvisual provider item
     public function show($id)
     {
-        return view('providers.show', compact('id'));
+        if ($id) {
+            $results = ApiHttpClient::request('get', 'providers/' . $id . '/show')->json();
+
+            if ($results['success'] == true) {
+                $provider = $results['data'];
+                // dd($provider);
+                return view('providers.show', compact('provider'));
+            } else {
+                session()->flash('type', 'Danger');
+                session()->flash('message', 'Something went wrong');
+                return redirect()->back();
+            }
+        } else {
+            session()->flash('type', 'Danger');
+            session()->flash('message', 'Something went wrong');
+            return redirect()->back();
+        }
     }
 
     public function allTrainer()
     {
         return 0;
+    }
+
+    public function enrollBatch($provider_id, Request $request)
+    {
+        if ($provider_id) {
+            $results = ApiHttpClient::request('get', 'providers/' . $provider_id . '/show')->json();
+
+            if ($results['success'] == true) {
+                $provider = $results['data'];
+                // dd($provider);
+                return view('providers.enroll_batches', compact('provider'));
+            } else {
+                session()->flash('type', 'Danger');
+                session()->flash('message', 'Something went wrong');
+                return redirect()->back();
+            }
+        } else {
+            session()->flash('type', 'Danger');
+            session()->flash('message', 'Something went wrong');
+            return redirect()->back();
+        }
     }
 }
