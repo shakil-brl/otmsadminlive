c8142d82ac5d39d0af23281c581e00f481dadcf4
SHakil Hossain <shakil@newgen-bd.com>
2023-12-27 11:24:45 +0600

    Update ongoing_classes.blade.php

--- resources/views/dashboard_details/ongoing_classes.blade.php
+++ resources/views/dashboard_details/ongoing_classes.blade.php
@@ -59,6 +59,19 @@
                                         {{__('batch-schedule.join_class')}}
                                     </a>
                                 @endif
+
+                                @php
+                                $inspection_pm = [
+                                'batch_id' => $batch['training_batch']['id'],
+                                'schedule_detail_id' => $batch['id'],
+                                ];
+                                @endphp
+                                <a class="btn btn-sm btn-primary" href="{{ route('tms-inspections.create', $inspection_pm) }}"
+                                target="_blank">
+                                Inspection
+                                </a>
+
+                                
                             </td>
                         </tr>
                     @endforeach
