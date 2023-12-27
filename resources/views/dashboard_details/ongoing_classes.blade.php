50b54fa213ae1ec00ef37b62d09b935d12f333a8
SHakil Hossain <shakil@newgen-bd.com>
2023-12-27 11:35:56 +0600

    Update ongoing_classes.blade.php

--- resources/views/dashboard_details/ongoing_classes.blade.php
+++ resources/views/dashboard_details/ongoing_classes.blade.php
@@ -59,12 +59,7 @@
                                         {{__('batch-schedule.join_class')}}
                                     </a>
                                 @endif
-                                @php
-                                $inspection_pm = [
-                                'batch_id' => $batch['training_batch']['id'],
-                                'schedule_detail_id' => $batch['id'],
-                                ];
-                                @endphp
+                            
 
                                  {{-- @php
                                 $inspection_pm = [
