6a3ebfe9a0a040e56b18fc9a1dbd2150a820e10e
Ashraful-NG <ashraful@newgen-bd.com>
2024-02-11 15:05:47 +0600

    product crud

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.auth-master')
+
+@section('content')
+    <div class="m-5">
+        <div class="d-flex justify-content-end align-items-center">
+            <a class="btn btn-lg btn-success" href="{{ route('products.create') }}">Create Product</a>
+        </div>
+        <h3>Product List</h3>
+        <x-alert />
+
+        @isset($results['data'])
+            <div class="my-3">
+                <div class="d-none">
+                    <form action="">
+                        <div class="w-50 d-flex gap-3">
+                            <input type="search" name="search" value="{{ request('search') }}" class="form-control w-75"
+                                placeholder="Search Product">
+                            <input type="submit" class="form-control btn btn-primary w-25" value="Search">
+                        </div>
+                    </form>
+                </div>
+                <table class="table table-bordered bg-white">
+                    <thead>
+                        <th>S.N.</th>
+                        <th>Name</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </thead>
+                    <tbody>
+                        @if (count($results['data']) > 0)
+                            @foreach ($results['data'] ?? [] as $index => $product)
+                                @php
+                                    $from = $results['from'];
+                                @endphp
+                                <tr>
+                                    <td>
+                                        {{ $from + $loop->iteration - 1 }}
+                                    </td>
+                                    <td>
+                                        {{ $product['name'] ?? '' }}
+                                    </td>
+                                    <td>
+                                        {{ $product['is_active'] ?? '' }}
+                                    </td>
+                                    <td class="me-0 d-flex gap-1">
+                                        <a href="{{ route('products.edit', $product['id']) }}" class="btn btn-sm btn-info">
+                                            Edit
+                                        </a>
+                                        <form action="{{ route('products.destroy', $product['id']) }}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-sm btn-danger delete-action">Delete</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td colspan="4" class="text-danger">No data found</td>
+                            </tr>
+                        @endif
+                    </tbody>
+                </table>
+                {!! $paginator->links() !!}
+            </div>
+        @endisset
+    </div>
+@section('scripts')
+    <script>
+        $(document).ready(function() {
+            $(document).on("click", ".delete-action", function(e) {
+                e.preventDefault();
+                const form = $(this).closest('form');
+
+                Swal.fire({
+                    title: "Are you sure?",
+                    text: "You won't be able to revert this!",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Yes, delete it!",
+                    cancelButtonText: "No, cancel!",
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        form.submit();
+                    }
+                });
+            });
+        });
+    </script>
+@endsection
+@endsection
