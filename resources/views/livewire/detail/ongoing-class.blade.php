bf969fcc374f875754a16294d1508945f406d021
tariqul-babylon <tariqul@newgen-bd.com>
2024-03-04 18:42:28 +0600

    d

--- resources/views/livewire/detail/ongoing-class.blade.php
+++ resources/views/livewire/detail/ongoing-class.blade.php
@@ -115,7 +115,7 @@
             </div>
 
         </div>
-        <x-ongoing-class-component :classes="$classes" :from="$from" />
+        {{-- <x-ongoing-class-component :classes="$classes" :from="$from" /> --}}
 
         {!! $paginator->links() !!}
     @endisset
