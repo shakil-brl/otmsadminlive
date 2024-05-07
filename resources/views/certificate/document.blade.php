154751adb026fc0c5e7b8c18b22aa14223ae2906
tariqul-babylon <tariqul@newgen-bd.com>
2024-05-07 17:42:30 +0600

    certificate

--- resources/views/certificate/document.blade.php
+++ resources/views/certificate/document.blade.php
@@ -2,58 +2,24 @@
 <html>
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
     <style>
         body {
-            font-family: Arial, sans-serif;
             margin: 0;
         }
 
         .certificate {
             width: 11.69in;
             height: 8.27in;
-            padding: 20px;
-            border: 1px solid #ccc;
-        }
-
-        .certificate-content {
-            margin-top: 170px;
-            border-width: 4px;
-            border-color: #6c3483;
-        }
-
-        .certificate-header {
+            padding: 0 1in;
             text-align: center;
-            margin-bottom: 20px;
-        }
-
-        .top-logos {}
-
-        .logo {
-            display: inline-block;
-            margin-right: 10px;
-        }
-
-        .certificate-title {
-            font-size: 24px;
-            margin-bottom: 10px;
-        }
-
-        .certificate-details {
-            text-align: center;
-            padding: 10px;
-        }
-
-        .certificate-details p {
-            margin: 0;
-        }
-
-        .certificate-signatures {
-            width: 100%;
-        }
-
-        .signature {
-            text-align: center;
-            margin-top: 20px;
+            background-image: url('img/certificate/certificate-bg.png');
+            background-size: 100%;
+            background-repeat: no-repeat;
+            font-family: "flex";
         }
     </style>
 </head>
@@ -62,33 +28,50 @@
     {{-- clock.png --}}
     {{-- img/certificate/certificate-bg.png --}}
 
-    <div class="certificate"
-        style="background-image: url('img/certificate/certificate-bg.png') ; background-size: 100%; background-repeat: no-repeat;">
-
-        <div class="certificate-content">
-            <div class="certificate-header">
-                <div class="top-logos">
-
-                </div>
+    @foreach (range(1, 2) as $item)
+        <div class="certificate">
+            <div style="font-size: 27pt; padding-top: 200px; ">
+                Certificate of Completion
             </div>
-            <div class="certificate-details">
-                <h1 class="certificate-title">Certificate of Completion</h1>
-                <h2>[Name]</h2>
-                <p>son/daughter of [Parent's Name]</p>
-                <p>has successfully completed the [Course Duration] course on</p>
-                <h3>"Digital Marketing"</h3>
-                <p>Conducted by [Institute Name] and supported by [Supporting Organization]</p>
+            <div style="font-size: 12pt; padding-top: 40px; ">
+                This is to certify that
             </div>
-            <div class="certificate-signatures" style="">
-                <div class="signature" style="display: inline-block">
-                    <p>[Signatory Title 1]</p>
-                </div>
-                <div class="signature" style="display: inline-block">
-                    <p>[Signatory Title 2]</p>
-                </div>
+            <div style="font-size: 27pt; padding-top: 14px; font-family: 'dflix'">
+                Sadia Afreen Sweety
             </div>
+            <div style="font-size: 12pt; padding-top: 32px;line-height: 1.5  ">
+                Son / Daughter of Md. Selim Hossain
+            </div>
+            <div style="font-size: 12pt; line-height: 1.5;  ">
+                has successfully completed the 528 hours course on
+            </div>
+            <div style="font-size: 23pt; padding-top: 15px;  ">
+                “Digital Marketing”
+            </div>
+            <div style="font-size: 12pt; padding-top: 10px; padding-bottom: 50px; line-height: 1.5;  ">
+                conducted by babylon resources limited and supported by ict division.
+            </div>
+            <table style="width: 100%; ">
+                <tr>
+                    <td style="font-size: 8pt; line-height: 1.15; text-align: center;font-family: 'flex'; ">
+                        <img width="90" src="img/certificate/sign1.png" alt=""> <br>
+                        Md. Mostafa Kamal <br>
+                        Establishment of Sheikh Kamal IT Training <br>
+                        & Incubation Center project
+                    </td>
+                    <td style=" ">
+                        QR Code
+                    </td>
+                    <td style="font-size: 8pt; line-height: 1.15; text-align: center; font-family: 'flex';">
+                        <img width="90" src="img/certificate/sign2.png" alt=""> <br>
+                        Md. Mostafa Kamal <br>
+                        Establishment of Sheikh Kamal IT Training <br>
+                        & Incubation Center project
+                    </td>
+                </tr>
+            </table>
         </div>
-    </div>
+    @endforeach
 </body>
 
 </html>
