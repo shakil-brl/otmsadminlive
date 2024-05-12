95671d9b03881146bc3144de8cbffb80d950ae7d
Ashraful-NG <ashraful@newgen-bd.com>
2024-05-12 12:36:36 +0600

    c

--- resources/views/verify/warning.blade.php
+++ resources/views/verify/warning.blade.php
@@ -6,13 +6,10 @@
             <div class="text-center mb-3">
                 <div class="badge-warning">
                     <img src="{{ asset('img/icon/warning.png') }}" alt="" class="text-center badge-img">
-                    Unvarified Certificate
+                    Certificate Not Found
                 </div>
             </div>
             <div>
-                <div style="font-size: 20px;" class="text-danger">
-                    ID <b>{{ $id ?? '' }}</b> not found. <br>
-                </div>
                 <a href="{{ url($url ?? '') }}" style="background: #2c004e;" class="btn btn-primary rounded-pill mt-3">Search
                     Again</a>
             </div>
