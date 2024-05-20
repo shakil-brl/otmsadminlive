64196fbecb4d563c7c6b892a18a349108d43f7ba
tariqul-babylon <tariqul@newgen-bd.com>
2024-05-20 16:10:22 +0600

    action button label change

--- resources/views/livewire/detail/total-batch.blade.php
+++ resources/views/livewire/detail/total-batch.blade.php
@@ -202,7 +202,7 @@
                                 <div class="dropdown">
                                     <button class="btn btn-secondary dropdown-toggle" type="button"
                                         id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                        All Actions
+                                        Action
                                     </button>
                                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                                         @if ($batch['schedule']['total_complete'] || $batch['schedule']['total_pending'] || $batch['schedule']['total_running'])
